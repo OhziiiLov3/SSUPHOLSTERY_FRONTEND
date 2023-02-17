27187d2b884733a8f1327c44c19f627c568bb592
ohziiilov3 <klbaskerville0520@gmail.com>
2023-02-17 14:38:43 -0800

    feat: editscreen, action, store, reducer added and working on frontend

--- src/screens/UserEditScreen.js
+++ src/screens/UserEditScreen.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { Link, useLocation, useNavigate, useParams} from "react-router-dom";
-import { Form, Button, Container } from "react-bootstrap";
+import { Form, Button, Container, Alert } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import Loader from "../components/Loader";
 import Message from "../components/Message";
-import { getUserDetails } from "../actions/userActions";
+import { getUserDetails, updateUser } from "../actions/userActions";
+import {USER_UPDATE_RESET} from '../constants/userContstants'
 
 const UserEditScreen = () => {
     const params = useParams();
@@ -21,19 +22,30 @@ const UserEditScreen = () => {
   const userDetails = useSelector((state) => state.userDetails);
   const { error, loading, user } = userDetails;
 
-  useEffect(() => {
-     if(!user.name || user._id !== Number(userId)){
-        dispatch(getUserDetails(userId))
-     }else{
-        setName(user.name)
-        setEmail(user.email)
-        setIsAdmin(user.isAdmin)
-     }
-  }, [user, userId]);
+
+  const userUpdate= useSelector((state) => state.userUpdate);
+  const { error: errorUpdate, loading: loadingUpdate, success: successUpdate } = userUpdate;
+
+  useEffect(() => { 
+
+      if(successUpdate){
+        dispatch({type:USER_UPDATE_RESET})
+        navigate('/admin/userlist')
+      }else{
+        if (!user.name || user._id !== Number(userId)) {
+          dispatch(getUserDetails(userId));
+        } else {
+          setName(user.name);
+          setEmail(user.email);
+          setIsAdmin(user.isAdmin);
+        }
+      }
+     
+  }, [user, userId, successUpdate, navigate]);
 
   const submitHandler = (e) => {
     e.preventDefault();
-    
+    dispatch(updateUser({_id:user._id, name , email, isAdmin}))
   };
 
   return (
@@ -43,10 +55,13 @@ const UserEditScreen = () => {
       </Link>
       <Container className="bg-dark my-5 p-3">
         <h1 className="mx-auto py-3">Edit User</h1>
+        {loadingUpdate && <Loader />}
+        {errorUpdate && <Alert varaint="danger">{errorUpdate}</Alert>}
+
         {loading ? (
           <Loader />
         ) : error ? (
-          <Message varaint="danger">{error}</Message>
+          <Alert varaint="danger">{error}</Alert>
         ) : (
           <Form variant="light" onSubmit={submitHandler}>
             <Form.Group className="mb-3 py-3" controlId="name">
